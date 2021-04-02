8ec7eb9139907a3dd3c6f6a684f68ce27edd7a3c
rajasyah <rajasyahacp20@gmail.com>
2021-04-02 20:30:11 +0700

    menambalikan seperti default

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Home from "./component/Home";
-import Nav from "./component/Nav";
-import Tentang from "./component/Tentang";
-import Gallery from "./component/Gallery";
-import Contact from "./component/Contact";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/Tentang" component={Tentang} />
-          <Route path="/Gallery" component={Gallery} />
-          <Route path="/Contact" component={Contact} />
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <h1>Hello world</h1>
+    </div>
   );
 }
 
